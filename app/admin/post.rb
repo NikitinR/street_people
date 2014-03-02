ae7a1e5f55d89b9f46ff4007ca5771a710367097
Ramzes.nro@gmail.com <Ramzes.nro@gmail.com>
2014-03-02 18:16:52 +0200

    image to gallery

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -14,6 +14,6 @@ ActiveAdmin.register Post do
   #  permitted
   # end
 
-  permit_params :title, :text
+  permit_params :title, :text, :image
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
   validates :title, presence: true,
             length: { minimum: 5 }
 end
