2c6771e5daa6c4e29451d6fcd1cbba847161941a
Ramzes.nro@gmail.com <Ramzes.nro@gmail.com>
2014-03-07 01:43:50 +0200

    add page team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,14 @@
 class TeamsController < ApplicationController
+  def index
+    @teams = Team.all
+  end
+
+
+
+  private
+  def team_params
+    params.require(:team).permit(:name, :info, :photo)
+  end
+
+
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,5 @@
 class Team < ActiveRecord::Base
+  mount_uploader :photo, PhotoUploader
+  validates :name, presence: true
+  validates :info, presence: true
 end
