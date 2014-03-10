bc1bf3af57b3d5129855069006d4f3c87760cb55
Andrey Repin <aarepin@gmail.com>
2014-03-10 21:57:18 +0200

    minor changes

--- db/migrate/20140305230239_remove_columns_from_posts.rb
+++ db/migrate/20140305230239_remove_columns_from_posts.rb
@@ -1,12 +1,5 @@
 class RemoveColumnsFromPosts < ActiveRecord::Migration
   def change
     remove_column :posts, :thumbnail, :string
-    remove_column :posts, :thumbnail_file_name, :string
-    remove_column :posts, :thumbnail_content_type, :string
-    remove_column :posts, :thumbnail_file_size, :string
-    remove_column :posts, :thumbnail_updated_at, :string
-    remove_column :posts, :portrait, :string
-    remove_column :posts, :avatar, :string
-    remove_column :posts, :avatar_file_name, :string
   end
 end
