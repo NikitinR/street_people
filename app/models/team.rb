ab5f760634aeb5a693518607ee7f711b5f4800b3
Ramzes.nro@gmail.com <Ramzes.nro@gmail.com>
2014-03-30 02:18:11 +0200

    change imeges uploaders

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
-  mount_uploader :photo, PhotoUploader
+  mount_uploader :photo, ImageUploader
   validates :name, presence: true
   validates :info, presence: true
 end
